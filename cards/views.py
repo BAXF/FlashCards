19af0b9be54c8141c9a7d54fead3b53462e80c29
Madvin <m.parvin986@gmail.com>
2024-03-23 04:04:30 +0330

    add random card functions

--- cards/views.py
+++ cards/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
-# Create your views here.
+from .models import Card
+
+def easy_random_card(request):
+    card = Card.objects.filter(level='easy').order_by('?').first()
+    return render(request, 'card.html', {'card': card})
+
+def intermediate_random_card(request):
+    card = Card.objects.filter(level='intermediate').order_by('?').first()
+    return render(request, 'card.html', {'card': card})
+
+def advanced_random_card(request):
+    card = Card.objects.filter(level='advanced').order_by('?').first()
+    return render(request, 'card.html', {'card': card})
