50c830c7fabe0057e2cb251fd5767291144ed853
Madvin <m.parvin986@gmail.com>
2024-03-23 21:09:19 +0330

    add home

--- cards/urls.py
+++ cards/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('easy', views.easy_random_card, name='easy_random_card'),
     path('intermediate', views.intermediate_random_card, name='intermediate_random_card'),
     path('advanced', views.advanced_random_card, name='advanced_random_card'),
+    path('', views.home, name='home')
 ]
