3d010f7e1918b720f5ad3d2732a846bcb0e14b91
Madvin <m.parvin986@gmail.com>
2024-03-31 16:20:07 +0330

    add card group

--- cards/urls.py
+++ cards/urls.py
@@ -2,8 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('easy', views.easy_random_card, name='easy_random_card'),
-    path('intermediate', views.intermediate_random_card, name='intermediate_random_card'),
-    path('advanced', views.advanced_random_card, name='advanced_random_card'),
-    path('', views.home, name='home')
+    path('', views.home, name='home'),
+    path('<str:group>/', views.get_random_card, name='group'),
 ]
