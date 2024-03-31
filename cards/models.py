4f9d9920a3c6c70a49106c6ac58c8de37738b1d5
Madvin <m.parvin986@gmail.com>
2024-03-31 16:19:09 +0330

    add cardgroup

--- cards/models.py
+++ cards/models.py
@@ -2,25 +2,34 @@ from django.db import models
 import uuid
 from django.urls import reverse
 
+    
+class CardGroup(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=255, unique=True, null=False, blank=False)
+    image_url = models.URLField(null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        ordering = ['-created_at']
+
 class Card(models.Model):
     class STATUS_CHOICES(models.TextChoices):
         DRAFT = 'draft', 'Draft'
         PUBLISHED = 'published', 'Published'
         ARCHIVED = 'archived', 'Archived'
 
-    class LEVEL_CHOICES(models.TextChoices):
-        EASY = 'easy', 'Easy'
-        INTERMEDIATE = 'intermediate', 'Intermediate'
-        ADVANCED = 'advanced', 'Advanced'
-
     uuid = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     name = models.CharField(max_length=255)
+    group = models.ForeignKey('CardGroup', on_delete=models.CASCADE, null=True, blank=True)
     description = models.TextField()
     image_url = models.URLField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     user = models.ForeignKey('auth.User', on_delete=models.CASCADE)
-    level = models.CharField(max_length=15, choices=LEVEL_CHOICES.choices, default=LEVEL_CHOICES.EASY)
     status = models.CharField(max_length=10, choices=STATUS_CHOICES.choices, default=STATUS_CHOICES.DRAFT)
 
     class Meta:
@@ -28,4 +37,3 @@ class Card(models.Model):
 
     def __str__(self):
         return self.name
-    
