9363dc3485e26f5e43e13795e8c0cd41c5e55410
Madvin <m.parvin986@gmail.com>
2024-03-31 16:18:41 +0330

    add site header,title and register cardgroup

--- cards/admin.py
+++ cards/admin.py
@@ -1,5 +1,10 @@
 from django.contrib import admin
 
-from .models import Card
+from .models import Card, CardGroup
+
+admin.site.site_header = 'FlashCard Admin'
+admin.site.site_title = 'FlashCard Admin Area'
+admin.site.index_title = 'Welcome to the FlashCard Admin Area'
 
 admin.site.register(Card)
+admin.site.register(CardGroup)
